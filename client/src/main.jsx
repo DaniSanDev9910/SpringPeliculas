222dd41d6c38fd949c642b837f72a95ba6e856ab
Yixsi <yixsi3103@gmail.com>
2025-06-11 22:26:18 -0500

    remove redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,22 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-import { configureStore } from "@reduxjs/toolkit";
 
 import App from "./App";
 
 // Create a minimal Redux store. Replace reducers as the application grows.
-const store = configureStore({ reducer: {} });
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
-  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </Provider>
 );
 
 
