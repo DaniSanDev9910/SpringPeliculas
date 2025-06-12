b8a30ff822275279872a7b454b275d75f0018cf8
Yixsi <yixsi3103@gmail.com>
2025-06-11 22:24:50 -0500

    use router

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "@reduxjs/toolkit";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from "./App";
+
+// Create a minimal Redux store. Replace reducers as the application grows.
+const store = configureStore({ reducer: {} });
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
+
+
